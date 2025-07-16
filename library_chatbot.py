461e457416f7f4f8d84e00ce52bc4d367b9e65ed
hyunju75 <nulnul16022@gmail.com>
2025-07-16 23:52:30 +0900

    Update library_chatbot.py

--- library_chatbot.py
+++ library_chatbot.py
@@ -58,7 +58,7 @@ def get_vectorstore(_docs):
 # PDF 문서 로드-벡터 DB 저장-검색기-히스토리 모두 합친 Chain 구축
 @st.cache_resource
 def initialize_components(selected_model):
-    file_path = r"/mount/src/library_chatbot/[챗봇프로그램및실습] 부경대학교 규정집.pdf"
+    file_path = r"/mount/src/library_chatbot/2024학년도 졸업요건 안내자료.pdf"  #1
     pages = load_and_split_pdf(file_path)
     vectorstore = get_vectorstore(pages)
     retriever = vectorstore.as_retriever()
@@ -99,7 +99,7 @@ def initialize_components(selected_model):
     return rag_chain
 
 # Streamlit UI
-st.header("국립부경대 도서관 규정 Q&A 챗봇 💬 📚")
+st.header("국립부경대 졸업요건 Q&A 챗봇 💬 📚")  #2
 option = st.selectbox("Select GPT Model", ("gpt-4o-mini", "gpt-3.5-turbo-0125"))
 rag_chain = initialize_components(option)
 chat_history = StreamlitChatMessageHistory(key="chat_messages")
@@ -115,7 +115,7 @@ conversational_rag_chain = RunnableWithMessageHistory(
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", 
-                                     "content": "국립부경대 도서관 규정에 대해 무엇이든 물어보세요!!!"}]
+                                     "content": "국립부경대 졸업 규정에 대해 무엇이든 물어보세요!!!"}] #3
 
 for msg in chat_history.messages:
     st.chat_message(msg.type).write(msg.content)
